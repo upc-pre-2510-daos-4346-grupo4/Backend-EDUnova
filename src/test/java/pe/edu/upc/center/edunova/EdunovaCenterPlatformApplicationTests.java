3e25cc95a93082c301ec6dafaa0134252e2429e5
Diego A <u202221436@upc.edu.pe>
2025-07-10 13:08:55 -0500

    Tu mensaje de commit

---
+++ src/test/java/pe/edu/upc/center/edunova/EdunovaCenterPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.center.edunova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdunovaCenterPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
