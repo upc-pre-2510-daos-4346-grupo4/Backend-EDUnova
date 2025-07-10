4fbde70f96be24f70531465dc6ab967d674386cf
Diego A <u202221436@upc.edu.pe>
2025-07-10 13:32:29 -0500

    Cambios finales para todos

--- src/main/java/pe/edu/upc/center/edunova/EdunovaCenterPlatformApplication.java
+++ src/main/java/pe/edu/upc/center/edunova/EdunovaCenterPlatformApplication.java
@@ -2,8 +2,10 @@ package pe.edu.upc.center.edunova;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EdunovaCenterPlatformApplication {
 
     public static void main(String[] args) {
